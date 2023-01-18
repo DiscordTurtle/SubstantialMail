d193249c52f801087ca4044b4b2828eb20fbd5e2
Daan Steenbergen <thadutchgameryt@gmail.com>
2023-01-18 11:51:10 +0100

    Add button prints any available unread email
    It also now fetches all mails continuously but only prints one when the button is pressed

--- main.py
+++ main.py
@@ -2,22 +2,38 @@ import button
 import printer
 import signal
 import mail
+import time
+
+latest_email: mail.ReceivedMail = None
+amount_unread: int = 0
 
 def button_callback():
+    global amount_unread
     print("Button pressed")
 
-    latest = mail.get_last_unseen()
-    with open("latest.txt", "w") as file:
-        # Write subject and sender to text file
-        file.write(f"Subject: {latest.subject}\n")
-        file.write(f"From: {latest.sender}\n")
-        file.write(latest.body)
-    printer.print("latest.txt")
+    if latest_email == None:
+        print("No email available")
+    else:
+        with open("latest.txt", "w") as file:
+            # Write subject and sender to text file
+            file.write(f"Subject: {latest_email.subject}\n")
+            file.write(f"From: {latest_email.sender}\n")
+            body = latest_email.get_body_and_mark_read()
+            if body != None:
+                file.write(body)
+        printer.print("latest.txt")
+        amount_unread -= 1
+        print(f"Printed latest email and marked it as read: {amount_unread}")
 
 def main():
+    global latest_email, amount_unread
     button.set_callback(button_callback)
-    signal.pause()
-    mail.close()
+    while True:
+        unread_indexes = mail.get_unread_indexes()
+        amount_unread = len(unread_indexes)
+        if (amount_unread > 0):
+            latest_email = mail.read_message_by_index(unread_indexes[-1])       
+        time.sleep(5)
 
 if __name__ == "__main__":
     main()
