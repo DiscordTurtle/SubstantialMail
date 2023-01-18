daac0cbd3fbd40d093e3c7abdc55201e42f4ec56
Daan Steenbergen <thadutchgameryt@gmail.com>
2023-01-18 11:51:10 +0100

    Add button prints any available unread email
    It also now fetches all mails continuously but only prints one when the button is pressed

--- mail.py
+++ mail.py
@@ -15,19 +15,33 @@ imap.login(USERNAME, PASSWORD)
 class ReceivedMail:
     sender: str
     subject: str
-    body: str
+    _index: int
+    _body: str
 
-def get_last_unseen() -> ReceivedMail:
+    def get_body_and_mark_read(self) -> str:
+        if self._body == None:
+            status, message = imap.fetch(str(self._index),'(RFC822)')
+
+            response = message[0]
+            msg = email.message_from_bytes(response[1])
+            if msg.is_multipart():
+                for part in msg.walk():
+                    if msg.get_content_type() == "text/plain" and "attachment" not in part.get("Content-Disposition"):
+                        self._body = part.get_payload(decode=True).decode()
+            else:
+                if msg.get_content_type() == "text/plain":
+                    self._body = msg.get_payload(decode=True).decode()
+        return self._body
+    
+
+def get_unread_indexes() -> list:
     status, count = imap.select("INBOX")
     status, message_nums = imap.search(None, "UNSEEN")
 
-    messages = re.findall(r"\d+", str(message_nums))
+    return re.findall(r"\d+", str(message_nums))
 
-    amount_unread = len(messages)
-    newest_unread = messages[-1]
-
-    status, message = imap.fetch(str(newest_unread), "(RFC822)")
-    print(message)
+def read_message_by_index(index) -> ReceivedMail:
+    status, message = imap.fetch(str(index),'(BODY.PEEK[])')
     if (len(message) <= 0):
         return None
     else:
@@ -44,19 +58,8 @@ def get_last_unseen() -> ReceivedMail:
         sender, encoding = decode_header(msg.get("From"))[0]
         if isinstance(sender, bytes):
             sender = sender.decode(encoding)
-
-        body = ""
-        if msg.is_multipart():
-            for part in msg.walk():
-                if msg.get_content_type() == "text/plain" and "attachment" not in part.get("Content-Disposition"):
-                    body = part.get_payload(decode=True).decode()
-        else:
-            if msg.get_content_type() == "text/plain":
-                body = msg.get_payload(decode=True).decode()
-        return ReceivedMail(sender, subject, body)
-
-def set_last_as_read():
-    pass
+            
+        return ReceivedMail(sender, subject, index, None)
 
 def close():
     imap.close()
