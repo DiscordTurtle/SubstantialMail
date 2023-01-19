df0ed91d1e6a8efc4a3bdb919ba5c91dd0a8be28
CristianFildan <72780371+CristianFildan@users.noreply.github.com>
2023-01-19 13:10:49 +0100

    Add files via upload

--- printer.py
+++ printer.py
@@ -5,5 +5,15 @@ PRINTER_NAME = "Epson"
 conn = cups.Connection()
 
 def print(file_name):
-    conn.printFile(PRINTER_NAME, file_name, "", {})
+    with open(file_name, 'r') as file:
+        text = file.read()
+    
+    new_text = ""
+    count = 0
+    for char in text:
+        new_text += char
+        count += 1
+        if count % 25 == 0:
+            new_text += "\n"
+    conn.printFile(PRINTER_NAME, new_text, "", {})
     
